75cfd8647a617051cff38a80dec2217e7c98caad
andresm220 <123190520+andresm220@users.noreply.github.com>
2025-04-27 21:18:04 -0600

    Update constants.dart

--- EduVial/lib/config/constants.dart
+++ EduVial/lib/config/constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants{
-  static const String apiBaseUrl = 'https://dev.eduvial.space/';  // Android emulator
+  static const String apiBaseUrl = 'https://dev.eduvial.space';  // Android emulator
   static const String loginEndpoint = '$apiBaseUrl/api/auth/login';
   static const String registerEndpoint = '$apiBaseUrl/api/auth/register';
 
