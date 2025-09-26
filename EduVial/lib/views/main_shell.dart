21ad9f3bd99ee6deab7913a26e1bcf4803424a6f
djuarez-2017510 <djuarez-2017510@kinal.edu.gt>
2025-09-25 19:14:50 -0600

    feat: Welcome added

--- EduVial/lib/views/main_shell.dart
+++ EduVial/lib/views/main_shell.dart
@@ -1,23 +1,24 @@
+// main_shell.dart
 import 'package:flutter/material.dart';
 import 'package:eduvial/views/menu.dart';
 import 'package:eduvial/views/ranking_screen.dart';
 import 'package:eduvial/views/laws_screen.dart';
 
 import 'package:eduvial/models/user.dart';
-import 'package:eduvial/controllers/auth_controller.dart'; // <- usa auth_controller
+import 'package:eduvial/controllers/auth_controller.dart';
+import 'package:eduvial/widgets/welcome_overlay.dart'; // 👈 overlay modular
 
 class MainShell extends StatefulWidget {
   const MainShell({super.key});
-
   @override
   State<MainShell> createState() => _MainShellState();
 }
 
 class _MainShellState extends State<MainShell> {
   int _index = 0;
-
   String? _token;
   User? _me;
+  bool _welcomeShown = false;
 
   @override
   void initState() {
@@ -30,22 +31,24 @@ class _MainShellState extends State<MainShell> {
     User? me = await auth_controller.loadCachedUser();
     me ??= await auth_controller.refreshMeAndCache();
 
-    setState(() {
-      _token = token;
-      _me = me;
-    });
+    if (!mounted) return;
+    setState(() { _token = token; _me = me; });
+
+    // Mostrar bienvenida UNA sola vez
+    if (!_welcomeShown) {
+      _welcomeShown = true;
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        showWelcomeDialog(context); // 👈 listo
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // Mientras carga token/usuario
     if (_token == null || _me == null) {
-      return const Scaffold(
-        body: Center(child: CircularProgressIndicator()),
-      );
+      return const Scaffold(body: Center(child: CircularProgressIndicator()));
     }
 
-    // ¡OJO! Ya no es const porque RankingScreen recibe parámetros dinámicos
     final pages = [
       const Menu(),
       RankingScreen(token: _token!, me: _me!),
